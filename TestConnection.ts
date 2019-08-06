1fb2873927544db8c005c3101e04bc5193ca20b4
Simon Mika <simonmika@users.noreply.github.com>
2019-08-06 18:25:55 +0200

    Removed reference to jest. (#6)

--- TestConnection.ts
+++ TestConnection.ts
@@ -1,9 +1,5 @@
-// tslint:disable-next-line: no-implicit-dependencies
 import { MongoMemoryServer } from "mongodb-memory-server"
 import { Connection } from "./Connection"
-
-// Extend the default timeout so MongoDB binaries can download
-jest.setTimeout(60000)
 
 export class TestConnection extends Connection {
 	constructor(private server: MongoMemoryServer) {

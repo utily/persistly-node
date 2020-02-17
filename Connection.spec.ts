89900831546335d82c678014355124bf778c8d4d
Simon Mika <simonmika@users.noreply.github.com>
2020-02-17 21:43:08 +0100

    Project configuration and test. (#8)
    * Added test.
    
    * Added editorconfig file.
    
    * Updated dependencies.

--- Connection.spec.ts
+++ Connection.spec.ts
@@ -1,7 +1,18 @@
 import * as persistly from "."
+
 describe("Connection", () => {
 	it("open non-existing", () => {
 		const connection = persistly.Connection.open("mongodb://localhost/nonexisting")
 		expect(connection).toBeTruthy()
+	})
+	it("open real", async () => {
+		const db = process.env.database
+		if (db) {
+			const connection = persistly.Connection.open(db)
+			expect(connection).toBeTruthy()
+			const merchant = await connection.get<{ id: string } & any>("merchant", "id", 4)
+			expect(merchant).toBeTruthy()
+			await connection.close()
+		}
 	})
 })

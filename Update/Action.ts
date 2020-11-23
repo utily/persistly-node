9f545ba172d11b56a6502287e5906587f778ddfc
florianebmeier <70569666+florianebmeier@users.noreply.github.com>
2020-11-23 09:58:07 +0100

    Initial addToSet (#30)

--- Update/Action.ts
+++ Update/Action.ts
@@ -2,18 +2,20 @@ export type Action<T> = {
 	$set?: T
 	$unset?: true
 	$push?: T
+	$addToSet?: T
 }
 
 export namespace Action {
 	export function is(value: any | Action<any>): value is Action<any> {
 		return (
-			typeof value == "object" && (value.$set != undefined || value.$unset != undefined || value.$push != undefined)
+			typeof value == "object" &&
+			(value.$set != undefined || value.$unset != undefined || value.$push != undefined || value.$addToSet != undefined)
 		)
 	}
-	export type Operator = "$set" | "$unset" | "$push"
+	export type Operator = "$set" | "$unset" | "$push" | "$addToSet"
 	export namespace Operator {
 		export function is(value: any | Operator): value is Operator {
-			return typeof value == "string" && ["$set", "$unset", "$push"].some(o => o == value)
+			return typeof value == "string" && ["$set", "$unset", "$push", "$addToSet"].some(o => o == value)
 		}
 	}
 	export function extract<T>(action: Action<T> | any): Action<T> | undefined {
@@ -25,6 +27,8 @@ export namespace Action {
 				result.$unset = action.$unset
 			if ("$push" in action)
 				result.$push = action.$push
+			if ("$addToSet" in action)
+				result.$addToSet = action.$addToSet
 		}
 		return result
 	}
--- Update/index.spec.ts
+++ Update/index.spec.ts
@@ -11,4 +11,25 @@ describe("Update", () => {
 		const filter = persistly.Update.toMongo(argument)
 		expect(filter).toEqual({ $set: { name: "test", field: 1337, "property.nested": 42 }, $unset: { remove: true } })
 	})
+	it("addToSet", () => {
+		const argument: persistly.Update<{
+			name: string
+			field: number
+			property: { nested: number }
+			remove: number
+			event: number[]
+		}> = {
+			name: "test",
+			field: { $set: 1337 },
+			property: { nested: 42 },
+			remove: { $unset: true },
+			event: { $addToSet: [1, 2] },
+		}
+		const filter = persistly.Update.toMongo(argument)
+		expect(filter).toEqual({
+			$set: { name: "test", field: 1337, "property.nested": 42 },
+			$unset: { remove: true },
+			$addToSet: { event: [1, 2] },
+		})
+	})
 })
